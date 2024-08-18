5ce39eecab1d286c500c71d371029f07e94cf017
kanaxz <kanaxstratz@gmail.com>
2024-08-18 23:49:25 +0200

    1.1.0

--- index.js
+++ index.js
@@ -7,16 +7,22 @@ const { join } = require('path')
 const helmet = require('helmet')
 
 module.exports = {
-  dependencies: ['sools-core'],
+  name: 'express',
+  dependencies: ['core'],
   construct({ core }, config) {
     const expressApp = express()
 
-    expressApp.use((req, res, next) => {
-      next()
+    expressApp.get('ping', (req, res) => {
+      res.send('pong')
     })
 
     expressApp.use(cors({
-      origin: true,
+      origin: (origin, callback) => {
+        console.log(origin)
+        const success = origin.startsWith(`https://${config.express.host}`)
+        callback(success ? null : new Error('Not allowed'), success)
+      },
+      credentials: true
     }))
 
     expressApp.use(cookieParser())
@@ -41,7 +47,7 @@ module.exports = {
       expressApp.use('/*', (req, res) => {
         res.sendFile(join(config.dist, 'index.html'))
       })
-      server.listen(config.express.port)
+      server.listen(config.express.port, '127.0.0.1')
     })
 
     return expressApp
