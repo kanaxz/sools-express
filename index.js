eea84571a18970fe21507763d12164f4e7dd5323
kanaxz <kanaxstratz@gmail.com>
2024-08-19 10:36:19 +0200

    1.5.0

--- index.js
+++ index.js
@@ -3,9 +3,7 @@ const bodyParser = require('body-parser')
 const cookieParser = require('cookie-parser')
 const https = require('https')
 const http = require('http')
-const cors = require('cors')
 const { join } = require('path')
-const helmet = require('helmet')
 
 module.exports = {
   name: 'express',
@@ -16,15 +14,6 @@ module.exports = {
     expressApp.get('ping', (req, res) => {
       res.send('pong')
     })
-
-    expressApp.use(cors({
-      origin: (origin, callback) => {
-        console.log(origin)
-        const success = origin.startsWith(`https://${config.host}`)
-        callback(success ? null : new Error('Not allowed'), success)
-      },
-      credentials: true
-    }))
 
     expressApp.use(cookieParser())
     expressApp.use(bodyParser.json())
